62844d826c26148d2fa6522173d611d77b2c91a6
Solo-LA36 <fin82.82@yandex.ru>
2025-10-19 16:17:53 +0300

    Initial commit

---
+++ src/test/java/pages/comonents/CalendarComponent.java
@@ -0,0 +1,24 @@
+package pages.comonents;
+
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
+
+public class CalendarComponent {
+    private final SelenideElement monthSelect = $(".react-datepicker__month-select");
+    private final SelenideElement yearSelect = $(".react-datepicker__year-select");
+
+
+    public void setDate(String day, String month, String year) {
+
+        monthSelect.selectOption(month);
+        yearSelect.selectOption(year);
+        $(".react-datepicker__day--0" + day).click();
+    }
+
+
+
+
+}
---
+++ src/test/java/tests/PracticeFormWithPageObjectsTests.java
@@ -0,0 +1,43 @@
+package tests;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import page.PracticeFormPages;
+import tests.pageobjectmodel.TestBase;
+import tests.pageobjectmodel.TestData;
+
+@Tag("demoqa")
+public class PracticeFormWithPageObjectsTests extends TestBase {
+    PracticeFormPages practiceFormPages = new PracticeFormPages();
+    TestData testData = new TestData();
+
+    @Test
+    void fillFormTest() {
+        practiceFormPages.openPage()
+                .removeAds()
+                .setFirstName(testData.firstName)
+                .setLastName(testData.lastName)
+                .setEmail(testData.email)
+                .setGender(testData.gender)
+                .setNumber(testData.mobileNumber)
+                .setDateOfBirth(testData.birthday[0], testData.birthday[1], testData.birthday[2])
+                .setSubjects(testData.subjects)
+                .setHobbies(testData.hobbies)
+                .uploadPicture(testData.uploadFile)
+                .setAddress(testData.address)
+                .selectStateAndCity(testData.stateAndCity[0], testData.stateAndCity[1])
+                .submitForm();
+
+        practiceFormPages.verifyResultModalAppears()
+                .verifyResult("Student Name", testData.expectedFullName)
+                .verifyResult("Student Email", testData.email)
+                .verifyResult("Gender", testData.gender)
+                .verifyResult("Mobile", testData.mobileNumber)
+                .verifyResult("Date of Birth", testData.expectedBirthday)
+                .verifyResult("Subjects", testData.subjects)
+                .verifyResult("Hobbies", testData.hobbies)
+                .verifyResult("Picture", testData.uploadFile)
+                .verifyResult("Address", testData.address)
+                .verifyResult("State and City", testData.expectedStateAndCity);
+    }
+}
